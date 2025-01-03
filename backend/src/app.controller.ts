ad6fb5cd133d89fd021907677626084a5501e708
Bryan Tapia <marcelotapia01@outlook.com>
2025-01-03 18:37:01 -0500

    feat(backend): add health check endpoint

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOperation({ summary: 'Health check' })
+  @ApiResponse({ status: 200, description: 'API is up and running' })
+  @Get('health-check')
+  getHealthCheck(): string {
+    return this.appService.getHealthCheck();
   }
 }
