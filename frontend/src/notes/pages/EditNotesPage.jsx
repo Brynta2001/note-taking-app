fd155448d04398ea58d449fae6cdc7e0777cd19c
Bryan Tapia <marcelotapia01@outlook.com>
2025-01-04 16:35:16 -0500

    feat(frontend): add feature to edit notes

---
+++ frontend/src/notes/pages/EditNotesPage.jsx
@@ -0,0 +1,10 @@
+import { NotesLayout } from '../layout/NotesLayout';
+import { EditNoteFormview } from '../views/EditNoteFormview';
+
+export const EditNotesPage = () => {
+  return (
+    <NotesLayout>
+      <EditNoteFormview />
+    </NotesLayout>
+  );
+};
